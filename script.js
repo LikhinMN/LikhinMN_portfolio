8cf28420289018737a73f5c14ce9afb9dc5d971a
LikhinMN <mnlikhin@gmail.com>
2024-11-24 14:43:51 +0530

    finish

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function togglemenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".hamburger-icon");
+function togglemenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var modes = document.getElementById("modes");
+var thememode = document.getElementById("thememode");
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        modes.src = "../assets/pngwing.com(2).png";
+        thememode.src = "../assets/pngwing.com(2).png";
+    } else {
+        modes.src = "../assets/pngwing.com.png";
+        thememode.src = "../assets/pngwing.com.png";
+    }
+}
+
+modes.onclick = toggleTheme;
+thememode.onclick = toggleTheme;
+
