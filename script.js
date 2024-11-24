a48437805d841b2b39c765722574f5ce0cd77774
LikhinMN <mnlikhin@gmail.com>
2024-11-24 17:33:24 +0530

    update javascript

--- script.js
+++ script.js
@@ -9,30 +9,30 @@ let darkMode = localStorage.getItem('darkMode');
 const enableDarkMode = () => {
     document.body.classList.add('dark-mode');
     localStorage.setItem('darkMode', 'active');
-    document.querySelector('img[alt="dark mode"]').style.display = 'none';
-    document.querySelector('img[alt="light mode"]').style.display = 'inline';
+    document.querySelectorAll('img[alt="dark mode"]').forEach(img => img.style.display = 'none');
+    document.querySelectorAll('img[alt="light mode"]').forEach(img => img.style.display = 'inline');
 };
 
 const disableDarkMode = () => {
     document.body.classList.remove('dark-mode');
     localStorage.setItem('darkMode', 'inactive');
-    document.querySelector('img[alt="dark mode"]').style.display = 'inline';
-    document.querySelector('img[alt="light mode"]').style.display = 'none';
+    document.querySelectorAll('img[alt="dark mode"]').forEach(img => img.style.display = 'inline');
+    document.querySelectorAll('img[alt="light mode"]').forEach(img => img.style.display = 'none');
 };
-
 
 if (darkMode === 'active') {
     enableDarkMode();
 }
 
-const themeSwitch = document.getElementById('theme-switch');
-themeSwitch.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode'); 
-    if (darkMode !== 'active') {
-        enableDarkMode();
-    } else {
-        disableDarkMode();
-    }
+const themeSwitches = document.querySelectorAll('#theme-switch');
+
+themeSwitches.forEach(themeSwitch => {
+    themeSwitch.addEventListener('click', () => {
+        darkMode = localStorage.getItem('darkMode'); 
+        if (darkMode !== 'active') {
+            enableDarkMode();
+        } else {
+            disableDarkMode();
+        }
+    });
 });
-
-
